2b432927fef66b81130b64b59ae6aeff41fc4186
Matheus Oliveira <matheus.oliveira.rabelo@gmail.com>
2015-06-24 14:47:30 -0300

    Welcome screen designed

---
+++ Tutor/TTWelcomeViewController.swift
@@ -0,0 +1,45 @@
+//
+//  TTWelcomeViewController.swift
+//  
+//
+//  Created by Matheus Oliveira Rabelo on 6/24/15.
+//
+//
+
+import UIKit
+
+class TTWelcomeViewController: UIViewController {
+
+    @IBOutlet weak var imgUserProfile: UIImageView!
+    @IBOutlet weak var btNext: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.imgUserProfile.layer.masksToBounds = true
+        self.imgUserProfile.layer.cornerRadius = self.imgUserProfile.frame.size.width / 2
+        self.imgUserProfile.layer.borderColor = UIColor.whiteColor().CGColor
+        self.imgUserProfile.layer.borderWidth = 3
+        
+        self.btNext.layer.masksToBounds = true
+        self.btNext.layer.cornerRadius = self.btNext.frame.size.height / 2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
