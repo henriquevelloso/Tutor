46378aa940df9a1ef831bcb534c5641bd6825b98
Matheus Oliveira <matheus.oliveira.rabelo@gmail.com>
2015-06-24 14:28:59 -0300

    AdamiralCAT font added, LauchScreen image, Login Screen designed & Login Screen animations.

--- Tutor/AppDelegate.swift
+++ Tutor/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        
         
         // [Optional] Power your app with Local Datastore. For more info, go to
         // https://parse.com/docs/ios_guide#localdatastore/iOS
@@ -31,13 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // [Optional] Track statistics around application opens.
         //PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         
-        
         //Facebook 4
         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-        
-        
-        
-        
     }
     
 
--- Tutor/ViewController.swift
+++ Tutor/ViewController.swift
@@ -10,23 +10,94 @@ import UIKit
  
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var constraintIconPos: NSLayoutConstraint!
+    @IBOutlet weak var constraintTitlePos: NSLayoutConstraint!
+    @IBOutlet weak var constraintSubtitlePos: NSLayoutConstraint!
 
+    @IBOutlet weak var imgIcon: UIImageView!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblSubtitle: UILabel!
+    
+    var initIconPosConst : CGFloat!
+    var initTitlePosConst : CGFloat!
+    var initSubtitlePosConst : CGFloat!
+    var btnLogin : FBSDKLoginButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-
-        var loginButton: FBSDKLoginButton = FBSDKLoginButton()
-        loginButton.center = self.view.center
-        self.view.addSubview(loginButton)
+        self.initIconPosConst = self.constraintIconPos.constant
+        self.initTitlePosConst = self.constraintTitlePos.constant
+        self.initSubtitlePosConst = self.constraintSubtitlePos.constant
         
-//        
-//        
-//        FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
-//        loginButton.center = self.view.center;
-//        [self.view addSubview:loginButton];
-//        
-//        
+        self.constraintIconPos.constant = -self.view.frame.size.height
+        self.constraintTitlePos.constant = -self.view.frame.size.height
+        self.constraintSubtitlePos.constant = -self.view.frame.size.height
+        
+        var loginPoint : CGPoint = self.view.center
+        loginPoint.y = self.view.frame.size.height + 100
+        
+        self.btnLogin = FBSDKLoginButton()
+        self.btnLogin.center = loginPoint
+        self.view.addSubview(self.btnLogin)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Animate Icon Image
+        self.constraintIconPos.constant = self.initIconPosConst + 8
+        UIView.animateWithDuration(0.6, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.imgIcon.layoutIfNeeded()
+            }, completion: { (completed) -> Void in
+                self.constraintIconPos.constant = self.initIconPosConst
+                UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                    self.imgIcon.layoutIfNeeded()
+                    }, completion: nil)
+            })
+        
+        
+        // Animate Title Label
+        self.constraintTitlePos.constant = self.initTitlePosConst + 8
+        UIView.animateWithDuration(0.6, delay: 0.2, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.lblTitle.layoutIfNeeded()
+            }, completion: { (completed) -> Void in
+                self.constraintTitlePos.constant = self.initTitlePosConst
+                UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                    self.lblTitle.layoutIfNeeded()
+                    }, completion: nil)
+        })
+        
+        // Animate Subtitle Label
+        self.constraintSubtitlePos.constant = self.initSubtitlePosConst + 8
+        UIView.animateWithDuration(0.6, delay: 0.4, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            self.lblSubtitle.layoutIfNeeded()
+            }, completion: { (completed) -> Void in
+                self.constraintSubtitlePos.constant = self.initSubtitlePosConst
+                UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                    self.lblSubtitle.layoutIfNeeded()
+                    }, completion: nil)
+        })
+        
+        // Animate Button
+        var buttonPoint : CGPoint = self.view.center
+        buttonPoint.y = buttonPoint.y + 150
+        
+        UIView.animateWithDuration(0.6, delay: 0.6, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+            
+            var loginPoint : CGPoint = buttonPoint
+            loginPoint.y = loginPoint.y - 8
+            
+            self.btnLogin.center = loginPoint
+            
+            }, completion: { (completed) -> Void in
+                UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                    self.btnLogin.center = buttonPoint
+                    }, completion: nil)
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +105,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
  
+    
 
 }
 
