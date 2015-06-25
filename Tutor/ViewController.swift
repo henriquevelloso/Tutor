f89480d5228c6d716e9283f13b518d704820def9
Matheus Oliveira <matheus.oliveira.rabelo@gmail.com>
2015-06-25 11:25:22 -0300

    New facebook login button

--- Tutor/ViewController.swift
+++ Tutor/ViewController.swift
@@ -10,19 +10,21 @@ import UIKit
  
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var constraintIconPos: NSLayoutConstraint!
     @IBOutlet weak var constraintTitlePos: NSLayoutConstraint!
     @IBOutlet weak var constraintSubtitlePos: NSLayoutConstraint!
-
+    @IBOutlet weak var constraintLoginButton: NSLayoutConstraint!
+    
     @IBOutlet weak var imgIcon: UIImageView!
     @IBOutlet weak var lblTitle: UILabel!
     @IBOutlet weak var lblSubtitle: UILabel!
+    @IBOutlet weak var btLogin: UIButton!
+    
     
     var initIconPosConst : CGFloat!
     var initTitlePosConst : CGFloat!
     var initSubtitlePosConst : CGFloat!
-    var btnLogin : FBSDKLoginButton!
+    var initButtonPosConst : CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,17 +33,15 @@ class ViewController: UIViewController {
         self.initIconPosConst = self.constraintIconPos.constant
         self.initTitlePosConst = self.constraintTitlePos.constant
         self.initSubtitlePosConst = self.constraintSubtitlePos.constant
+        self.initButtonPosConst = self.constraintLoginButton.constant
         
         self.constraintIconPos.constant = -self.view.frame.size.height
         self.constraintTitlePos.constant = -self.view.frame.size.height
         self.constraintSubtitlePos.constant = -self.view.frame.size.height
+        self.constraintLoginButton.constant = -self.view.frame.size.height
         
-        var loginPoint : CGPoint = self.view.center
-        loginPoint.y = self.view.frame.size.height + 100
-        
-        self.btnLogin = FBSDKLoginButton()
-        self.btnLogin.center = loginPoint
-        self.view.addSubview(self.btnLogin)
+        self.btLogin.layer.masksToBounds = true
+        self.btLogin.layer.cornerRadius = 3
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -56,7 +56,7 @@ class ViewController: UIViewController {
                 UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
                     self.imgIcon.layoutIfNeeded()
                     }, completion: nil)
-            })
+        })
         
         
         // Animate Title Label
@@ -82,30 +82,78 @@ class ViewController: UIViewController {
         })
         
         // Animate Button
-        var buttonPoint : CGPoint = self.view.center
-        buttonPoint.y = buttonPoint.y + 150
-        
+        self.constraintLoginButton.constant = self.initButtonPosConst + 8
         UIView.animateWithDuration(0.6, delay: 0.6, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-            
-            var loginPoint : CGPoint = buttonPoint
-            loginPoint.y = loginPoint.y - 8
-            
-            self.btnLogin.center = loginPoint
-            
+            self.btLogin.layoutIfNeeded()
             }, completion: { (completed) -> Void in
+                self.constraintLoginButton.constant = self.initButtonPosConst
                 UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
-                    self.btnLogin.center = buttonPoint
+                    self.btLogin.layoutIfNeeded()
                     }, completion: nil)
         })
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
- 
     
-
+    @IBAction func btLoginTouchUpInsideAction(sender: UIButton) {
+        /*PFFacebookUtils.logInInBackgroundWithReadPermissions(["public_profile", "user_friends", "email"]) {
+            (user: PFUser?, error: NSError?) -> Void in
+            if let user = user {
+                if user.isNew {
+                    println("User signed up and logged in through Facebook!")
+                    
+                    var request : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+                    request.startWithCompletionHandler({ (connection, result, error) -> Void in
+                        if let fb = result as? NSDictionary{
+                            
+                            // Facebook
+                            var fbid : String = fb.objectForKey("id") as! String
+                            var email : String = fb.objectForKey("email") as! String
+                            var name : String = fb.objectForKey("name") as! String
+                            var gender : String = fb.objectForKey("gender") as! String
+                            // locale = "pt_BR"
+                            
+                            var imgProfileLink : String = "https://graph.facebook.com/\(fbid)/picture?type=large&return_ssl_resources=1"
+                            let imgProfileURL = NSURL(string: imgProfileLink)
+                            let imgProfileData = NSData(contentsOfURL: imgProfileURL!)
+                            
+                            
+                            
+                            // Parse
+                            user.email = email
+                            user.setObject(fbid, forKey: "facebookId")
+                            user.setObject(name, forKey: "name")
+                            user.setObject(gender, forKey: "gender")
+                            
+                            if let imgProfileData = imgProfileData
+                            {
+                                var file : PFFile = PFFile(name: "default", data: imgProfileData)
+                                user.setObject(file, forKey: "image")
+                            }
+                            user.saveInBackgroundWithBlock({ (successed, error) -> Void in
+                                self.performSegueWithIdentifier("segueLoginToWelcome", sender: nil)
+                            })
+                        }
+                    })
+                    
+                    
+                } else {
+                    
+                    println("User logged in through Facebook!")
+                    self.performSegueWithIdentifier("segueLoginToWelcome", sender: nil)
+                }
+            } else {
+                println("Uh oh. The user cancelled the Facebook login.")
+            }
+        }*/
+    }
+    
+    
+    
+    
 }
 
