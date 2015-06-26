d9f40c8334d43a49e32f1d28726022da20122080
Matheus Oliveira <matheus.oliveira.rabelo@gmail.com>
2015-06-26 09:17:11 -0300

    AutoLayout select native language

--- Tutor/CadastreViewController.swift
+++ Tutor/CadastreViewController.swift
@@ -58,46 +58,36 @@ class CadastreViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         
+        let cell = tableView.dequeueReusableCellWithIdentifier("Language", forIndexPath: indexPath) as! UITableViewCell
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        
+        let label = cell.viewWithTag(1) as! UILabel
+        let languages = self.language[indexPath.row]["Name"] as! String // [indexPath.row/2]
+        label.text = languages
+        
+        let bodyView : UIView = cell.viewWithTag(2)!
+        
+        bodyView.layer.cornerRadius = 4
+        bodyView.layer.masksToBounds = true
         
         
-//        if (indexPath.row % 2 == 0) {
-        
-            let cell = tableView.dequeueReusableCellWithIdentifier("Language", forIndexPath: indexPath) as! UITableViewCell
-            cell.selectionStyle = UITableViewCellSelectionStyle.None
-            
-            let label = cell.viewWithTag(1) as! UILabel
-            let languages = self.language[indexPath.row]["Name"] as! String // [indexPath.row/2]
-            label.text = languages
-            
-            if(indexPath.row == self.languageNativeRow){
-                cell.backgroundColor = UIColor(red: 64/255, green: 148/255, blue: 74/255, alpha: 0.10)
-            }else{
-                cell.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.10)
-            }
-            
-            return cell
-//        }
-//        else {
-//            let cell2 = tableView.dequeueReusableCellWithIdentifier("Separate", forIndexPath: indexPath) as! UITableViewCell
-//            cell2.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0)
-//            return cell2
-//        }
+        if(indexPath.row == self.languageNativeRow){
+            bodyView.backgroundColor = UIColor(red: 64/255, green: 148/255, blue: 74/255, alpha: 0.5)
+        }else{
+            bodyView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        }
         
         
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let cell = self.tableViewLanguage.cellForRowAtIndexPath(indexPath)
-        //let image = cell?.viewWithTag(2) as! UIImageView
-        
-        //image.image = UIImage(named: "check")
-        cell?.backgroundColor = UIColor(red: 64/255, green: 148/255, blue: 74/255, alpha: 0.10)
         
         self.languageNative = self.language[indexPath.row] as? PFObject
         self.languageNativeRow = indexPath.row
         self.tableViewLanguage.reloadData()
-        
-        //            image.image = UIImage(named: "uncheck")
         
     }
     @IBAction func saveParseLanguageNative(sender: UIButton) {
